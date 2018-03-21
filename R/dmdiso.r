f7aa6c6e2ea7861bafe2e504b87d5bc3ea73aef5
Martin Ritchie <mritchie@fs.fed.us>
2018-03-20 17:14:58 -0700

    Right now, this fixes dmd.iso for use.metric. Need to do some more testing but seems pretty good so far.

--- R/dmdiso.r
+++ R/dmdiso.r
@@ -2,13 +2,14 @@
 dmd.iso<-function(ineq         = 2,
                   v.at         = NULL,
                   range.x      = NULL,
-                  max.sdi      = 450,
+                  max.sdi      = NULL,
                   reineke.term = 1.60,
                   vty          = 2,
                   vcex         = 0.75,
                   vcol         = "blue",
                   show.vol     = TRUE,
-                  v.ann        = TRUE){
+                  v.ann        = TRUE,
+                  use.metric   = FALSE){
 
 if(!(ineq %in% c(2, 3, 6, 7, 9))){
   message("Invalid equation number provided to dmd.iso, iso-lines not rendered")
@@ -25,17 +26,36 @@ if(is.null(v.at)){
   return()
 }
 
-max.sdi <- switch(ineq,
-                  ifelse(max.sdi<=1000 & max.sdi>=300, max.sdi , 400),
-                  450,
-                  400,
-                  410,
-                  365,
-                  ifelse(!(max.sdi<=600 & max.sdi>=450), 550, max.sdi),
-                  600,
-                  800,
-                  700)
+if(is.null(max.sdi) & (ineq %in% c(1, 6))){
+  message("Limiting sdi not specified by max.sdi for dmd.iso, iso-lines not rendered")
+  return()
+}
 
+if(!use.metric){   # English Units
+  max.sdi <- switch(ineq,
+                    ifelse(max.sdi<=1000 & max.sdi>=300, max.sdi , 400),
+                    450,
+                    400,
+                    410,
+                    365,
+                    ifelse(!(max.sdi<=600 & max.sdi>=450), 550, max.sdi),
+                    600,
+                    800,
+                    700)
+} else{            # metric units
+  max.sdi <- switch(ineq,
+                    ifelse(max.sdi<=2470 & max.sdi>=741, max.sdi , 988),
+                    1112,
+                    988,
+                    1013,
+                    902,
+                    ifelse(!(max.sdi<=1482 & max.sdi>=1112), 1359, max.sdi),
+                    1482,
+                    1976,
+                    1729)
+}
+
+# volume functions
 vf2<-function(vvv, ttt){
     ivv <- (( 0.017*ttt )/( vvv  + 152 ))^(-1/2.8)
     return(ivv)
@@ -70,40 +90,82 @@ if(show.vol){
 
   range.x<-sort(range.x)
   tx<-range.x[1]:range.x[2]
+
   islp<-1/reineke.term
   # array of volumes to be produced in cubic feet per acre
   v.array<-sort(v.array)
   for(k in 1:length(v.array)){
+    if(!use.metric){
+      ivol<- switch(ineq,
+                    NULL,
+                    vf2(v.array[k], tx),
+                    vf3(v.array[k], tx),
+                    NULL,
+                    NULL,
+                    vf6(v.array[k], tx),
+                    vf7(v.array[k], tx),
+                    NULL,
+                    vf9(v.array[k], tx))
+      iaa  <- 10*(max.sdi/tx)^(islp)
+      qq   <- sum((iaa-ivol)>0)
+    }else{
+      txe <- tx*.404686 #convert to Englgish
+      v.arraye <- v.array*35.3147*.404686 #Convert volume to English
+      ivol<- switch(ineq,
+                    NULL,
+                    vf2(v.arraye[k], txe),
+                    vf3(v.arraye[k], txe),
+                    NULL,
+                    NULL,
+                    vf6(v.arraye[k], txe),
+                    vf7(v.arraye[k], txe),
+                    NULL,
+                    vf9(v.arraye[k], txe))
+      iaa  <- 10*((max.sdi*.404686)/txe)^(islp)
+      qq   <- sum((iaa-ivol)>0)
+    }
 
-    ivol<- switch(ineq,
-                  NULL,
-                  vf2(v.array[k], tx),
-                  vf3(v.array[k], tx),
-                  NULL,
-                  NULL,
-                  vf6(v.array[k], tx),
-                  vf7(v.array[k], tx),
-                  NULL,
-                  vf9(v.array[k], tx))
-    iaa  <- 10*(max.sdi/tx)^(islp)
-    qq   <- sum((iaa-ivol)>0)
     # now draw the volume iso line
-    graphics::lines(tx[1:qq], ivol[1:qq], type="l", col=vcol, lwd=0.5, lty=vty )
+    if(!use.metric){
+      graphics::lines(tx[1:qq], ivol[1:qq], type="l", col=vcol, lwd=0.5, lty=vty )
+    }else{
+      graphics::lines(tx[1:qq], ivol[1:qq]*2.54, type="l", col=vcol, lwd=0.5, lty=vty )
+    }
     # now annotate the volume
     if(k==length(v.array)){
+
       if(v.ann){
-        graphics::text(range.x[2]*1.15, ivol[qq]*1.18,  "Volume", cex=vcex, col=vcol)
-        graphics::text(range.x[2]*1.15, ivol[qq]*1.09,
+        if(!use.metric){
+          graphics::text(range.x[2]*1.15, ivol[qq]*1.18,  "Volume", cex=vcex, col=vcol)
+          graphics::text(range.x[2]*1.15, ivol[qq]*1.09,
                        expression("ft"^3~"acre"^-1), cex=vcex, col=vcol)
+        }else{
+          graphics::text(range.x[2]*1.15, 2.54*ivol[qq]*1.18,  "Volume", cex=vcex, col=vcol)
+          graphics::text(range.x[2]*1.15, 2.54*ivol[qq]*1.09,
+                         expression("m"^3~"ha"^-1), cex=vcex, col=vcol)
+        }
       }
     }
     if(v.ann){
-      graphics::text(range.x[2]*1.15, ivol[qq], as.character(v.array[k]), cex=vcex, col=vcol)
+      if(!use.metric){
+        graphics::text(range.x[2]*1.15,
+                       ivol[qq], as.character(v.array[k]), cex=vcex, col=vcol)
+      }else{
+        graphics::text(range.x[2]*1.15,
+                       2.54*ivol[qq], as.character(v.array[k]), cex=vcex, col=vcol)
+      }
     }
     # now draw a segment linking the line and annotation if needed
     if(min( iaa ) < ivol[qq] ){
-      graphics::segments(tx[qq],   ivol[qq], range.x[2]*0.98,
+      if(!use.metric){
+        graphics::segments(tx[qq],   ivol[qq], range.x[2]*0.98,
                          ivol[qq],  col=vcol, lwd=0.5,   lty=1)
+      }else{
+        graphics::segments(tx[qq],   2.54*ivol[qq],
+                           range.x[2]*0.98,
+                           2.54*ivol[qq],
+                           col=vcol, lwd=0.5,   lty=1)
+      }
     }
   }
 }
